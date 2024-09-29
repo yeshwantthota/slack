b5dd6b2cb8f5932a37eca8bccf405f6abd9e3119
yeshwantthota <thotayeshwant@gmail.com>
2024-09-29 11:30:28 +0530

    provider authentication done

---
+++ convex/schema.ts
@@ -0,0 +1,7 @@
+import { defineSchema } from "convex/server";
+import { authTables } from "@convex-dev/auth/server";
+const schema = defineSchema({
+    ...authTables
+})
+
+export default schema;
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { convexAuthNextjsMiddleware, createRouteMatcher, nextjsMiddlewareRedirect } from "@convex-dev/auth/nextjs/server";
+ 
+const isPublicPage = createRouteMatcher(["/auth"])
+
+ 
+export default convexAuthNextjsMiddleware((request, { convexAuth }) => {
+     if(!isPublicPage(request) && !convexAuth.isAuthenticated()){
+        return nextjsMiddlewareRedirect(request, "/auth");
+     }
+
+     if(isPublicPage(request) && convexAuth.isAuthenticated()){
+        return nextjsMiddlewareRedirect(request, "/");
+     }
+});
+ 
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
